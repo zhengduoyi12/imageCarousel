32133f777f4a5fcfbd97b67141d829e1ec403aa4
alex zheng <zhengduoyi12@gmail.com>
2024-01-11 11:40:29 +1100

    feat:click changeIndex function and timer

--- src/components/imageCard.js
+++ src/components/imageCard.js
@@ -1,13 +1,13 @@
 
-const ImageCard = ({ props }) => {
-   
+const ImageCard = ({ props, current }) => {
+
     return (
-        <div className='slide current'>
-            <img src={ props.url } alt="" className="image"  />
-                < div className="content">
-                    <h1 className="title">{props.title}</h1>
-                    <div className="description">{props.description} </div>
-                </div>
+        <div className={current ? 'slide current' : 'slide'}>
+            <img src={props.url} alt="" className="image" />
+            < div className="content">
+                <h1 className="title">{props.title}</h1>
+                <div className="description">{props.description} </div>
+            </div>
         </div >
     )
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,6 @@
 import './index.scss';
 import { ImageCard } from '@/components/imageCard';
+import { useState, useEffect } from 'react';
 function App() {
     const imageList = [
         {
@@ -21,15 +22,38 @@ function App() {
             url: 'http://images.pexels.com/photos/597909/pexels-photo-597909.jpeg'
         },
     ]
+    const [currentIndex, setCurrentIndex] = useState(0)
+    const changeIndex = (num) => {
+        let res = currentIndex + num
+        if (res < 0) {
+            setCurrentIndex(imageList.length - 1)
+            return
+        }
+        if (res > imageList.length - 1) {
+            setCurrentIndex(0)
+            return
+        }
+        else {
+            setCurrentIndex(res)
+            return
+        }
+    }
+    useEffect(() => {
+        const timer = setInterval(() => {
+            changeIndex(1)
+        }, 5000)
+        return () => {
+            clearInterval(timer)
+        }
+    })
     return (
 
         <div className="image-slider">
-            {imageList.map(item =>
-                <ImageCard props={item} key={item.id}/>
+            {imageList.map((item, index) =>
+                <ImageCard props={item} key={item.id} current={currentIndex === index} />
             )}
-            {/* <div id="prev">1 </div> */}
-            <i id="prev" class="fa-solid fa-chevron-left"></i>
-            <i id="next" class="fa-solid fa-chevron-right"></i>
+            <i id="prev" class="fa-solid fa-chevron-left" onClick={() => changeIndex(-1)}></i>
+            <i id="next" class="fa-solid fa-chevron-right" onClick={() => changeIndex(1)}></i>
         </div>
 
     )
