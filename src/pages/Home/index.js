bba36415b5f9797b2f006fc4a3ddfd6b58ee5fe0
alex zheng <zhengduoyi12@gmail.com>
2024-01-11 13:26:38 +1100

    docs:list techs used in this project

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,6 @@
 import './index.scss';
 import { ImageCard } from '@/components/imageCard';
-import { useState, useEffect,useMemo } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { loadIndex } from '@/utils/load';
 import { useImageList } from '@/hooks/useImageList';
 
@@ -8,7 +8,7 @@ function App() {
     const { imageList } = useImageList();
     const [currentIndex, setCurrentIndex] = useState(0)
     //load 3 pic [current-1, current, current+1] 
-    const calculation = useMemo(() => loadIndex(currentIndex,imageList.length), [currentIndex,imageList.length]);
+    const calculation = useMemo(() => loadIndex(currentIndex, imageList.length), [currentIndex, imageList.length]);
     const changeIndex = (num) => {
         let res = currentIndex + num
         if (res < 0) {
@@ -37,7 +37,7 @@ function App() {
 
         <div className="image-slider">
             {imageList.map((item, index) =>
-                <ImageCard props={item} key={item.id} current={currentIndex === index} loadArray={calculation} index={index}/>
+                <ImageCard props={item} key={item.id} current={currentIndex === index} loadArray={calculation} index={index} />
             )}
             <i id="prev" className="fa-solid fa-chevron-left" onClick={() => changeIndex(-1)}></i>
             <i id="next" className="fa-solid fa-chevron-right" onClick={() => changeIndex(1)}></i>
